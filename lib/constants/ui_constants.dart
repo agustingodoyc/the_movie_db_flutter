036cf6a552b4a97979c5f6eb554519756811685d
Agustín Godoy <109157659+agustingodoyc@users.noreply.github.com>
2023-09-29 10:05:56 -0300

    Revert "App Creation"

--- lib/constants/ui_constants.dart
+++
@@ -1,7 +0,0 @@
-class UIConstants {
-  static const backdropLeftPadding = 90.0;
-  static const imagePadding = 20.0;
-  static const titleFontSize = 25.0;
-  static const textsLargePadding = 20.0;
-  static const textsShortPadding = 10.0;
-}
